f4f2ca5d23511f4f9b429768aa47c9965c61aa51
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-05-14 18:20:59 +1000

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// sidenav mobile
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelector('.sidenav');
+    var elems = M.Sidenav.init(elems);
+  });
+// slider
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelector('.slider');
+    var elems = M.Slider.init(elems);
+  });
